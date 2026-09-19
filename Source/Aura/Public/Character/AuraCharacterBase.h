4dbef53e8687da3ff78b05efd4487d9a63c9bedf
Shan <shantanu.mantri7@gmail.com>
2026-09-19 15:17:30 -0400

    add character folder

---
+++ Source/Aura/Public/Character/AuraCharacterBase.h
@@ -0,0 +1,22 @@
+// Copyright Shantanu Mantri
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "AuraCharacterBase.generated.h"
+
+UCLASS(ABSTRACT)
+class AURA_API AAuraCharacterBase : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	
+	AAuraCharacterBase();
+
+protected:
+	
+	virtual void BeginPlay() override;
+
+};
